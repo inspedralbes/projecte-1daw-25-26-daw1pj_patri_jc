37e969ec7eee51ad9064d620f392324b3d7ca3a1
a25juadiadua <a25juadiadua@inspedralbes.cat>
2026-04-24 13:30:56 +0200

    crear incidencia en proceso

---
+++ projecte/php/crear_inicidencies.php
@@ -0,0 +1,16 @@
+<?php 
+
+    //Per obtenir la connexió
+    require_once 'connexio.php';
+
+
+    function crear_incidencia($conn){
+
+        //Obtenir la incidencia
+        $nomIncidencia = $_POST['nom'];
+
+        if(empty($nom)){
+            echo "<p class='error'>El nom de la incidència no pot estar buit.</p>";
+            return;
+        }
+    }
